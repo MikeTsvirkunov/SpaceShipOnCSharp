cdf5b28d0183d68291134b67466ec3cabd03a0ca
Mike Tsvirkunov <mike.tsvirkunov@gmail.com>
2022-10-02 01:06:21 +0600

    first_test_commit

--- SpaceShipTest/UnitTest1.cs
+++ SpaceShipTest/UnitTest1.cs
@@ -25,8 +25,6 @@ namespace SpaceShipTest
         [TestMethod]
         public void obj_with_speed_and_without_coord()
         {
-            int[] expectation_coords = new int[2] { 5, 8 };
-
             MoveableObject spaceship_w_speed_and_coords = new MoveableObject(new Dictionary<string, dynamic> { { "speed", new int[2] { -7, 3 } } });
 
             try
@@ -38,7 +36,23 @@ namespace SpaceShipTest
                 StringAssert.Contains(e.Message, MoveableObject.CoordExistError);
                 return;
             }
+            Assert.Fail("The expected exception was not thrown.");
+        }
 
+        [TestMethod]
+        public void obj_with_coord_and_without_speed()
+        {
+            MoveableObject spaceship_w_speed_and_coords = new MoveableObject(new Dictionary<string, dynamic> { { "coord", new int[2] { -7, 3 } } });
+
+            try
+            {
+                spaceship_w_speed_and_coords.frontmove();
+            }
+            catch (System.ArgumentException e)
+            {
+                StringAssert.Contains(e.Message, MoveableObject.SpeedExistError);
+                return;
+            }
             Assert.Fail("The expected exception was not thrown.");
         }
     }
