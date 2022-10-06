73dc80e75c88966ce9adbb83ed41f919d567c9d8
MikeTsvirkunov <mike.tsvirkunov@gmail.com>
2022-10-06 05:18:14 +0000

    On_moq3

--- SaceShips.Lib.Test/UnitTest1.cs
+++ SaceShips.Lib.Test/UnitTest1.cs
@@ -8,11 +8,17 @@ namespace SaceShips.Lib.Test;
 public class UnitTest1
 {
     [Fact]
-    public void Test1()
+    public void Test_InterfaceObject()
     {
         var spaceship_w_speed_and_coords = new Mock<InterfaceObject>();
-        int[] expectation_coords = new int[2] { 5, 8 };
-        spaceship_w_speed_and_coords.Setup(p => p.GetAllParams()).Returns(It.IsAny<ConcurrentDictionary<string, dynamic>>);
-        
+        spaceship_w_speed_and_coords.Setup(p => p.GetAllParams()).Returns(It.IsAny<ConcurrentDictionary<string, dynamic>>());
+        spaceship_w_speed_and_coords.Setup(p => p.GetParam(It.IsAny<string>())).Returns(It.IsAny<dynamic>());
+        spaceship_w_speed_and_coords.Setup(p => p.ParamExist(It.IsAny<string>())).Returns(It.IsAny<dynamic>());
+    }
+
+    [Fact]
+    public void Test_Movement()
+    {
+        var spaceship_w_speed_and_coords = new Mock<Movement>();
     }
 }
