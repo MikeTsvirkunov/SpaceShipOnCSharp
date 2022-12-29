f5de6492995a987d36a8a73ac736ea1bfbc9c51f
MikeTsvirkunov <mike.tsvirkunov@gmail.com>
2022-12-29 11:02:29 +0600

    No_worry_working

--- SaceShips.Lib/Interfaces/ISource.cs
+++ SaceShips.Lib/Interfaces/ISource.cs
@@ -4,6 +4,6 @@ using System.Collections.Generic;
 
 public interface ISource
 {
-    Dictionary<object, object> nexts { get; set; }
+    Dictionary<object, object?> nexts { get; set; }
     object step_forward(object args);
 }
