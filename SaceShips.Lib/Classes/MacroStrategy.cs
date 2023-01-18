92f870bfb444468522526bcf655e6d8697f04cf7
MikeTsvirkunov <mike.tsvirkunov@gmail.com>
2023-01-18 21:32:21 +0600

    By IoC added empty keypair

--- SaceShips.Lib/Classes/MacroStrategy.cs
+++ SaceShips.Lib/Classes/MacroStrategy.cs
@@ -14,9 +14,8 @@ public class MacroStartegy : SaceShips.Lib.Interfaces.IStartegy
 
     public object execute(object[] args)
     {
-        // var cmds = new List<SaceShips.Lib.Interfaces.ICommand>();
         var cmds = Hwdtech.IoC.Resolve<List<SaceShips.Lib.Interfaces.ICommand>>("SpaceShip.Lib.Get.EmptyICommandList");
-        var x = Hwdtech.IoC.Resolve<List<KeyValuePair<SaceShips.Lib.Interfaces.IStartegy, object[]>>>("SpaceShip.Lib.Get.KeyPairStrategyParamsList", strategies.Zip((List<object[]>)args[1], (a, b) => new KeyValuePair<SaceShips.Lib.Interfaces.IStartegy, object[]>(a, b)));
+        var x = Hwdtech.IoC.Resolve<List<KeyValuePair<SaceShips.Lib.Interfaces.IStartegy, object[]>>>("SpaceShip.Lib.Get.KeyPairStrategyParamsList", strategies.Zip((List<object[]>)args[1], (a, b) => {return new KeyValuePair<SaceShips.Lib.Interfaces.IStartegy, object[]>(a, b);}));
         x.ForEach(c => cmds.Add((SaceShips.Lib.Interfaces.ICommand)c.Key.execute((IUObject) args[0], c.Value)));
         return (object)cmds;
     }
