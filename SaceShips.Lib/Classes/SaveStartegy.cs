054ba59d3c3224a5ad95197453433c0af5bc678f
MikeTsvirkunov <mike.tsvirkunov@gmail.com>
2023-01-18 20:28:54 +0600

    By IoC wo Comments

--- SaceShips.Lib/Classes/SaveStartegy.cs
+++ SaceShips.Lib/Classes/SaveStartegy.cs
@@ -17,11 +17,10 @@ public class SaveStrategy : IStartegy
     public object execute(params object[] args)
     {
         try{
-            return Hwdtech.IoC.Resolve<object>("SpaceShip.Lib.Strategy.KeyPairStringObject", "success", this.strategy.execute(args));
+            return Hwdtech.IoC.Resolve<object>("SpaceShip.Lib.Get.KeyPairStringObject", "success", this.strategy.execute(args));
         }
         catch(Exception ex){
-            return Hwdtech.IoC.Resolve<object>("SpaceShip.Lib.Strategy.KeyPairStringObject", "error", ErrorRegister.get_solution(Hwdtech.IoC.Resolve<List<object>>("SpaceShip.Lib.Strategy.ListObject", (object)strategy, (object)ex)));
-            // return (object)(new KeyValuePair<string, object>("error", (object)ErrorRegister.get_solution(new List<object>() { (object)strategy, (object)ex })));
+            return Hwdtech.IoC.Resolve<object>("SpaceShip.Lib.Get.KeyPairStringObject", "error", ErrorRegister.get_solution(Hwdtech.IoC.Resolve<List<object>>("SpaceShip.Lib.Get.ListObject", (object)strategy, (object)ex)));
         }
     }
 }
