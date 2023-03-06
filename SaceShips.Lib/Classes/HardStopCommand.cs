eaf7cd6618bde3875a1a09f5c2f97f00754ab28a
MikeTsvirkunov <mike.tsvirkunov@gmail.com>
2023-03-06 10:59:35 +0600

    On_while

--- SaceShips.Lib/Classes/HardStopCommand.cs
+++ SaceShips.Lib/Classes/HardStopCommand.cs
@@ -13,7 +13,6 @@ public class HardStopServerThreadCommand : SaceShips.Lib.Interfaces.ICommand
     }
     public void action()
     {
-        this.thread.ChangeMethod(new EmptyStrategy());
-        // this.thread.Stop();
+        this.thread.Stop();
     }
 }
