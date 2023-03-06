e2dc3a6848a8d542981f42ef412e96f207801cf7
MikeTsvirkunov <mike.tsvirkunov@gmail.com>
2023-03-06 10:50:42 +0600

    On_while

--- SaceShips.Lib/Classes/ServerThreadStrategy.cs
+++ SaceShips.Lib/Classes/ServerThreadStrategy.cs
@@ -8,25 +8,24 @@ namespace SaceShips.Lib.Classes;
 public class ServerThreadStrategy: IStartegy{//, IMethodChangeable{
 
     SaceShips.Lib.Interfaces.IStartegy f;
-    // bool r = true;
+    bool r = true;
     Thread thread;
     BlockingCollection<SaceShips.Lib.Interfaces.ICommand> queue;
 // Gregory endruse основы многопоточного программирования
     public ServerThreadStrategy(SaceShips.Lib.Interfaces.IStartegy f, BlockingCollection<SaceShips.Lib.Interfaces.ICommand> x){
         this.f = f;
         this.queue = x;
-        this.thread = new Thread(() => {((SaceShips.Lib.Interfaces.ICommand)this.f.execute(this.queue)).action();});
-        // this.thread = new Thread(() => {while (this.r && this.queue.Count() > 0){((SaceShips.Lib.Interfaces.ICommand)(this.queue.Take())).action();}});
+        this.thread = new Thread(() => {while (this.r && this.queue.Count() > 0){((SaceShips.Lib.Interfaces.ICommand)(this.queue.Take())).action();}});
     }
 
     public void ChangeMethod(SaceShips.Lib.Interfaces.IStartegy f){
         this.f = f;
     }
 
-    // public void Stop()
-    // {
-    //     this.r = false;
-    // }
+    public void Stop()
+    {
+        this.r = false;
+    }
 
     public object execute(params object[] args){
         return (object)this.thread;
