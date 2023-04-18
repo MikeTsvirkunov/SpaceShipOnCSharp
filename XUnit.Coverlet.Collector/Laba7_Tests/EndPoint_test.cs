90978d17232adde501a549d12090cb48edaabc88
MikeTsvirkunov <mike.tsvirkunov@gmail.com>
2023-04-18 11:28:36 +0600

    Null_test

--- XUnit.Coverlet.Collector/Laba7_Tests/EndPoint_test.cs
+++ XUnit.Coverlet.Collector/Laba7_Tests/EndPoint_test.cs
@@ -38,4 +38,22 @@ public class EP
         Assert.Equal(game_command_message_for_test.game_id, results["game_id"]);
         Assert.Equal(game_command_message_for_test.args, results["args"]);
     }
+
+    [Fact]
+    public void EndPoint_test_if_null()
+    {
+        new Hwdtech.Ioc.InitScopeBasedIoCImplementationCommand().Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", Hwdtech.IoC.Resolve<object>("Scopes.New", Hwdtech.IoC.Resolve<object>("Scopes.Root"))).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.EndPoints.GameCommandMessageGetter", (object[] args) => new GameCommandMessageGetter()).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.Messages.GameCommandMessage", (object[] args) => new GameCommandMessage()).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.Strategies.GameCommandMessagePreprocessing", (object[] args) => new GameCommandMessagePreprocessingStrategy()).Execute();
+
+        GameCommandMessage game_command_message_for_test = Hwdtech.IoC.Resolve<GameCommandMessage>("SpaceShip.Lib.Messages.GameCommandMessage");
+
+        GameCommandMessageGetter end_point_for_test = Hwdtech.IoC.Resolve<GameCommandMessageGetter>("SpaceShip.Lib.EndPoints.GameCommandMessageGetter");
+        var results = (Dictionary<string, object>)end_point_for_test.get_message(game_command_message_for_test);
+        Assert.Equal(game_command_message_for_test.command, results["command"]);
+        Assert.Equal(game_command_message_for_test.game_id, results["game_id"]);
+        Assert.Equal(game_command_message_for_test.args, results["args"]);
+    }
 }
