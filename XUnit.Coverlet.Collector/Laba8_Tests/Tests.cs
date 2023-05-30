375b6d7405184565d786e8be8afd3ca424dc6dcb
MikeTsvirkunov <mike.tsvirkunov@gmail.com>
2023-05-30 08:21:57 +0600

    Fast

--- XUnit.Coverlet.Collector/Laba8_Tests/Tests.cs
+++ XUnit.Coverlet.Collector/Laba8_Tests/Tests.cs
@@ -35,7 +35,10 @@ public class GameExeCommandTest
                 game_timer.Start();
                 return x;
             }
-            else return empty_command.Object;
+            else {
+                game_timer.Reset();
+                return empty_command.Object;
+            }
         };
         var main_scope = Hwdtech.IoC.Resolve<object>("Scopes.New", Hwdtech.IoC.Resolve<object>("Scopes.Root"));
         Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", main_scope).Execute();
@@ -47,6 +50,52 @@ public class GameExeCommandTest
         Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.Game.Queue.GetCommand", (object[] args) => ((Queue<SaceShips.Lib.Interfaces.ICommand>)args[0]).Dequeue()).Execute();
         Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.Game.ExceptionHandler", (object[] args) => test_command_error.Object).Execute();
         Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.Game.Run", (object[] args) => {return continue_game_command_execution(args[0]);}).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", main_scope).Execute();
+        var sw = new Stopwatch();
+        sw.Start();
+        var test_queue = Hwdtech.IoC.Resolve<Queue<SaceShips.Lib.Interfaces.ICommand>>("SpaceShip.Lib.GameQueue");
+        test_queue.Enqueue(test_command.Object);
+        var game_exe_command_test = Hwdtech.IoC.Resolve<SaceShips.Lib.Interfaces.ICommand>("SpaceShip.Lib.GameExeCommand", test_queue, game_scope);
+        game_exe_command_test.action();
+        test_command.Verify(p => p.action());
+    }
+
+    [Fact]
+    public void stope_Game_ExeCommand_by_time_test()
+    {
+        new Hwdtech.Ioc.InitScopeBasedIoCImplementationCommand().Execute();
+        int game_time = 1000000;
+        var game_timer = new Stopwatch();
+        var empty_command = new Mock<SaceShips.Lib.Interfaces.ICommand>();
+        empty_command.Setup(p => p.action());
+        var test_command = new Mock<SaceShips.Lib.Interfaces.ICommand>();
+        test_command.Setup(p => p.action()).Verifiable();
+        var test_command_error = new Mock<SaceShips.Lib.Interfaces.ICommand>();
+        test_command_error.Setup(p => p.action()).Verifiable();
+        var continue_game_command_execution = (object x) =>
+        {
+            game_timer.Stop();
+            if (game_timer.ElapsedTicks < game_time)
+            {
+                game_timer.Start();
+                return x;
+            }
+            else
+            {
+                game_timer.Reset();
+                return empty_command.Object;
+            }
+        };
+        var main_scope = Hwdtech.IoC.Resolve<object>("Scopes.New", Hwdtech.IoC.Resolve<object>("Scopes.Root"));
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", main_scope).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.GameExeCommand", (object[] args) => new GameExeCommand(args[0], args[1])).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.GameQueue", (object[] args) => new Queue<SaceShips.Lib.Interfaces.ICommand>()).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.SetScope", (object[] args) => new StandartSetScopeCommand(args[0])).Execute();
+        var game_scope = Hwdtech.IoC.Resolve<object>("Scopes.New", main_scope);
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", game_scope).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.Game.Queue.GetCommand", (object[] args) => ((Queue<SaceShips.Lib.Interfaces.ICommand>)args[0]).Dequeue()).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.Game.ExceptionHandler", (object[] args) => test_command_error.Object).Execute();
+        Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.Game.Run", (object[] args) => { return continue_game_command_execution(args[0]); }).Execute();
         Hwdtech.IoC.Resolve<Hwdtech.ICommand>("Scopes.Current.Set", main_scope).Execute();
         // Hwdtech.IoC.Resolve<Hwdtech.ICommand>("IoC.Register", "SpaceShip.Lib.Game.ExceptionHandler", (object[] args) => new Stopwatch()).Execute();
         var sw = new Stopwatch();
